1b17a51e82877d2b57b5d980899e3c7dd20df620
takashiono2 <takashiono710@gmail.com>
2023-08-23 01:21:59 +0900

    secition7_83

--- section7/router-test/src/App.js
+++ section7/router-test/src/App.js
@@ -2,24 +2,28 @@ import Home from './pages/Home'
 import Information from './pages/Information'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import './App.css'
 
+//リンク先、リンク時に色を青に
 function App() {
+  const isAuthenticated = false;
+
   return (
     <>
     <nav>
       <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/information">Information</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink to ="/">Home</NavLink></li>
+        <li><NavLink to = "/information">Information</NavLink></li>
+        <li><NavLink to ="/contact">Contact</NavLink></li>
       </ul>
     </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/information" element={<Information />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path ="/" element={<Home />} />
+        <Route path ="/information" element={<Information />} />
+        <Route path ="/contact" element={<Contact />} />
+        <Route path ="/auth" element={isAuthenticated ? <Navigate to ="/contact" />:<Home />} />
+        <Route path ="*" element={<NotFound />} />
       </Routes>
     </>
   );
--- section7/router-test/src/pages/Information.js
+++ section7/router-test/src/pages/Information.js
@@ -1,9 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 const Information = () => {
+  const navigate = useNavigate();
+
+  const gotoContact = () =>{
+    navigate('/contact');
+  }
+
   return (
   <>
     <h2>
       Informationです。
     </h2>
+    <button onClick={gotoContact}>お問合せページ</button>
   </>
   )
 }
